8593bf910863186695215cbad428050bae06e0bd
pan12 <6729656+pan12@users.noreply.github.com>
2020-07-20 12:58:23 +0400

    Update models and migration

--- Share/Models/Message.cs
+++ Share/Models/Message.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,7 +10,9 @@ namespace WebChat.Models
     {
         public int Id { get; set; }
         public string Text { get; set; }
+        [Required]
         public DateTime PostedTime { get; set; }
+        [Required]
         public User Sender { get; set; }
     }
 }
--- Share/Models/User.cs
+++ Share/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,15 @@ namespace WebChat.Models
 {
     public class User
     {
+        [Key]
         public int Id { get; set; }
+        [Required, MinLength(3), MaxLength(20)]
         public string Nickname { get; set; }
+        public ICollection<Message> Messages { get; set; }
+
+        public User()
+        {
+            Messages = new List<Message>();
+        }
     }
 }
